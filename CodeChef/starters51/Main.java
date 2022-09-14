ac16b8852ff3d7692e54b50ab3eaaa9ca3178172
prathamesh patil <patilprathamesh696@gmail.com>
2022-09-14 16:44:34 +0530

    new problem

--- CodeChef/starters51/Main.java
+++ CodeChef/starters51/Main.java
@@ -1,6 +1,6 @@
 package CodeChef.starters51;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 }
@@ -32,6 +32,39 @@ class B {
                 System.out.println("Second");
             else
                 System.out.println("Any");
+        }
+    }
+}
+class C {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 1; i <= t; i++) {
+            HashMap<Integer,Integer> map = new HashMap<>();
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for(int j = 0; j < n; j++) {
+                arr[j] = in.nextInt();
+            }
+            for(int k = 0; k < n; k++) {
+                Integer x = map.get(arr[k]);
+                map.put(arr[k],((x == null)?1:x+1));
+            }
+            int max = Collections.max(map.values());
+//            System.out.println("max "+max);
+            int maxcount1 = 0;
+            for(Integer num : map.values()) {
+                if(num == max) {
+                    maxcount1++;
+//                    System.out.println(maxcount);
+                }
+//                System.out.print(num + " ");
+            }
+//            System.out.println();
+            if(maxcount1 == 1)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
         }
     }
 }
